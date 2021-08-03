5cb08055dfd7a26e8473d69c3f7157800ef6de27
Fandy Syaukati Putra <fandy.sp@gmail.com>
2021-08-03 08:36:37 +0700

    baru

--- app/build/generated/source/buildConfig/release/id/kotlin/hspbtool/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/id/kotlin/hspbtool/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "id.kotlin.hspbtool";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 5;
-  public static final String VERSION_NAME = "0.0005";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "0.0006";
 }
